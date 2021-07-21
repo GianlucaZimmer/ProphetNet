e264d43d1176d2d281724c1de854b322c683d8c7
Gianluca Zimmer <gianlucazimmer@gmail.com>
2021-07-21 15:10:08 +0200

    add: preprocessing

---
+++ ProphetNet_Multi/xprophetnet_preprocess_squad.sh
@@ -0,0 +1,12 @@
+wget https://msraprophetnet.blob.core.windows.net/prophetnet/release_checkpoints/prophetnet_multi.pt
+
+python xprophetnet_tokenize_squad.py
+
+fairseq-preprocess \
+--user-dir ./prophetnet \
+--task translation_prophetnet \
+--source-lang src --target-lang tgt \
+--trainpref ./squad/tokenized/en.train --validpref ./squad/tokenized/en.dev --testpref ./squad/tokenized/en.test \
+--destdir ./squad/processed --srcdict prophetnet_multi_dict/dict.txt --tgtdict prophetnet_multi_dict/dict.txt \
+--workers 15
+
